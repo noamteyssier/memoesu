f10179ed66e51822151d2439855c9d2764d5b5f8
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-03-01 18:17:47 -0800

    added logging and comments

--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,23 @@ pub struct Cli {
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
+
+    // Load the graph.
     let graph = io::load_graph(&cli.input)?;
+
+    eprintln!("----------------------------------------");
+    eprintln!("Log");
+    eprintln!("----------------------------------------");
+    eprintln!(">> Number of nodes         : {}", graph.node_count());
+    eprintln!(">> Number of edges         : {}", graph.edge_count());
+
+
+    // Enumerate the subgraphs.
+    let now = std::time::Instant::now();
     let canon_counts = esu::enumerate_subgraphs(&graph, cli.subgraph_size);
+    eprintln!(">> Finished enumeration in : {:?}", now.elapsed());
+
+    // Write the results to the output file.
     io::write_counts(&canon_counts, cli.subgraph_size, cli.output)?;
     Ok(())
 }
