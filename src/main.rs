54909a2a09ffa78ac40e3ffb924843451977c8da
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-03-02 16:27:58 -0800

    include threaded version and pass to relevant esu

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ mod bitgraph;
 mod esu;
 mod io;
 mod ngraph;
+mod parallel_esu;
 mod walker;
 
 use anyhow::Result;
 use clap::Parser;
+use esu::enumerate_subgraphs;
+use parallel_esu::parallel_enumerate_subgraphs;
 
 #[derive(Parser, Debug)]
 pub struct Cli {
@@ -20,6 +23,10 @@ pub struct Cli {
     /// Number of subgraphs to find in the input graph
     #[arg(short, long)]
     subgraph_size: usize,
+
+    /// Number of threads to use (default: 1)
+    #[arg(short, long)]
+    threads: Option<usize>,
 }
 
 fn main() -> Result<()> {
@@ -36,7 +43,14 @@ fn main() -> Result<()> {
 
     // Enumerate the subgraphs.
     let now = std::time::Instant::now();
-    let canon_counts = esu::enumerate_subgraphs(&graph, cli.subgraph_size);
+    let canon_counts = if let Some(num_threads) = cli.threads {
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(num_threads)
+            .build_global()?;
+        parallel_enumerate_subgraphs(&graph, cli.subgraph_size)
+    } else {
+        enumerate_subgraphs(&graph, cli.subgraph_size) 
+    };
     eprintln!(">> Finished enumeration in : {:?}", now.elapsed());
 
     // Write the results to the output file.
