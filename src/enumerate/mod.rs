26d9304bcc1784d5987d20e282bf3f02a4541e60
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-03-15 18:44:49 -0700

    added enumerate and parallel to pub

--- src/enumerate/mod.rs
+++ src/enumerate/mod.rs
@@ -1,7 +1,11 @@
 mod bitgraph;
+mod esu;
 mod ngraph;
+mod parallel_esu;
 mod result;
 
 pub use bitgraph::BitGraph;
+pub use esu::enumerate_subgraphs;
 pub use ngraph::NautyGraph;
+pub use parallel_esu::parallel_enumerate_subgraphs;
 pub use result::EnumResult;
