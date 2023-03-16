669b24180fcf10f45e3eea8e3c014f4a771a72bd
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-03-15 18:39:33 -0700

    remove unused modules from tree

--- src/enumerate/mod.rs
+++ src/enumerate/mod.rs
@@ -1,15 +1,7 @@
 mod bitgraph;
-// mod esu;
-// mod multibitset;
 mod ngraph;
-// mod parallel_esu;
 mod result;
-// mod walker;
 
 pub use bitgraph::BitGraph;
-// pub use esu::enumerate_subgraphs;
-// use multibitset::MultiBitSet;
 pub use ngraph::NautyGraph;
-// pub use parallel_esu::parallel_enumerate_subgraphs;
 pub use result::EnumResult;
-// use walker::Walker;
