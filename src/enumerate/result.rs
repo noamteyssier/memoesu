c9ea49862f44babd52d54b488e058b32acdd0ead
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-06-27 13:09:25 -0700

    create struct to hold group results

--- src/enumerate/result.rs
+++ src/enumerate/result.rs
@@ -25,3 +25,26 @@ impl EnumResult {
         self.canon_counts.len()
     }
 }
+
+pub struct GroupResult {
+    groups: Groups,
+    num_total_subgraphs: usize,
+    num_unique_subgraphs: usize,
+}
+impl GroupResult {
+    pub fn new(groups: Groups, num_total_subgraphs: usize, num_unique_subgraphs: usize) -> Self {
+        Self { groups, num_total_subgraphs, num_unique_subgraphs }
+    }
+
+    pub fn groups(&self) -> &Groups {
+        &self.groups
+    }
+
+    pub fn total_subgraphs(&self) -> usize {
+        self.num_total_subgraphs
+    }
+
+    pub fn unique_subgraphs(&self) -> usize {
+        self.num_unique_subgraphs
+    }
+}
